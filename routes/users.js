00e8bd74a079bab84666a28d761b38e8d33e97e1
Calvin <calvin.peak@capslock.tw>
2016-10-09 23:59:05 +0900

    Add terminate sign back.

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,18 @@
-var express = require('express')
-var router = express.Router()
+var express = require('express');
+var router = express.Router();
 
 /* GET users listing. */
 router.get('/', (req, res, next) => {
+  res.redirect('/');
+});
 
-})
-
-router.get('/auth/gitbook', passport.authenticate('gitbook'))
+router.get('/auth/gitbook', passport.authenticate('gitbook'));
 
 router.get('/auth/gitbook/callback',
   passport.authenticate('gitbook', { failureRedirect: '/login' }),
   (req, res) => {
     // Successful authentication, redirect home.
-    res.redirect('/')
-  })
+    res.redirect('/');
+  });
 
-module.exports = router
+module.exports = router;
