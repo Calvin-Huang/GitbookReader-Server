5cfa3ad132c3848c2027022b07196e5e6b18c144
Calvin <calvin.peak@capslock.tw>
2016-10-09 23:22:38 +0900

    Initialize ORM module.

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,16 @@ var router = express.Router()
 
 /* GET users listing. */
 router.get('/', (req, res, next) => {
-  res.send('respond with a resource')
+
 })
+
+router.get('/auth/gitbook', passport.authenticate('gitbook'))
+
+router.get('/auth/gitbook/callback',
+  passport.authenticate('gitbook', { failureRedirect: '/login' }),
+  (req, res) => {
+    // Successful authentication, redirect home.
+    res.redirect('/')
+  })
 
 module.exports = router
