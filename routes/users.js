e8120e6c8ca42cf5ea717a891a5e4476a4ba44ef
Calvin <calvin.peak@capslock.tw>
2016-10-12 08:43:47 +0900

    Complete fetch starred function.

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var passport = require('passport');
+var fetch = require('node-fetch');
+var cheerio = require('cheerio');
 var router = express.Router();
 
 import { User } from '../models';
@@ -9,6 +11,30 @@ import { authenticate } from '../middlewares/authenticate';
 /* GET users listing. */
 router.get('/', authenticate, async (req, res, next) => {
   res.json(req.user)
+});
+
+router.get('/:username/starred', async (req, res, next) => {
+  const html = await fetch(`https://www.gitbook.com/@${req.params.username}/starred`)
+    .then((response) => {
+      return response.text()
+    });
+
+  const $ = cheerio.load(html);
+
+  const result = $('#AuthorProfile .Books:first-child .Book')
+    .map((i, book) => {
+      const anchor = $(book).find('h4.title > a');
+      const bookInfos = anchor.attr('href').split('/');
+      return {
+        id: bookInfos[bookInfos.length - 1],
+        author: bookInfos[bookInfos.length - 2],
+        title: anchor.text(),
+        description: $(book).find('p.description').text()
+      };
+    })
+    .get();
+
+  res.json(result);
 });
 
 router.get('/auth/gitbook', passport.authenticate('gitbook'));
