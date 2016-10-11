c46879c1f0f71a1a6dbd12ea424180e87269da90
Calvin <calvin.huang@capslock.tw>
2016-10-11 08:00:02 +0000

    complete signin by gitbook flow.

--- routes/users.js
+++ routes/users.js
@@ -2,18 +2,21 @@ var express = require('express');
 var passport = require('passport');
 var router = express.Router();
 
+import { User } from '../models';
+
 /* GET users listing. */
-router.get('/', (req, res, next) => {
-  res.redirect('/');
+router.get('/', async (req, res, next) => {
+  const user = await User.findOne({ token: req.query.token })
+  res.send(JSON.stringfy(user));
 });
 
 router.get('/auth/gitbook', passport.authenticate('gitbook'));
 
-router.get('/auth/gitbook/callback',
+router.all('/auth/gitbook/callback',
   passport.authenticate('gitbook', { failureRedirect: '/login' }),
   (req, res) => {
     // Successful authentication, redirect home.
-    res.redirect('/');
+    res.redirect(`/users?token=${req.user.token}`);
   });
 
 module.exports = router;
