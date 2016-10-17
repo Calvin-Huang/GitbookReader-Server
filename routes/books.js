339a177e9f0df635493c6d4a840a3a6042c49793
Calvin <calvin.peak@capslock.tw>
2016-10-18 00:41:50 +0900

    Trim html to fetch api json.

--- routes/books.js
+++ routes/books.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var fetch = require('node-fetch');
 var router = express.Router();
 
 import { User, Book } from '../models';
@@ -51,6 +52,34 @@ router.put('/', authenticate, async (req, res, next) => {
     }
   } else {
     res.json({ success: false, message: 'Parameter not found.' })
+  }
+});
+
+router.get('/search', async (req, res, next) => {
+  try {
+    const html = await fetch(`https://www.gitbook.com/search?q=${req.query.q}`)
+      .then((response) => {
+        return response.text();
+      });
+
+    const searchResultRawJSON = $.text()
+      .replace(/[.\s\S]*window\.Pages\.Search,\sprops:\s(.*)/i, "$1")
+      .replace(/(.*)\s}\),document\.getElementById[.\s\S]*/i, "$1")
+      .replace(/(\n|\r)/, '');
+
+    if (searchResultRawJSON == '') {
+      throw Error('empty response');
+    }
+
+    if (!searchResultRawJSON.match(/^\{\"total\":\{\"books\":\d+,\"authors\":\d+},\"results\":{\"list\":\[[.\s\S]*\],\"total\":\d+,\"limit\":\d+,\"page\":\d+,\"pages\":\d+\},\"type\":\"\w+\",\"sort\":\"\w+\",\"config\":\{\"version\":\"[.\s\S]+\",\"hasBlog\":(true|false),\"isEnterprise\":(true|false),\"apiHost\":\"[.\s\S]+\",\"mainHost\":\"[.\s\S]+\",\"stripePublicKey\":\"[.\s\S]+\",\"gaToken\":\"[.\s\S]+\"\},\"req\":\{\"date\":\"[.\s\S]+\",\"csrfToken\":\"[.\s\S]+\",\"query\":\{\"q\":\"\w+\"\},\"url\":\"[.\s\S]+\",\"duration\":\d+\}\}/i)) {
+      throw Error(searchResultRawJSON);
+    } else {
+
+    }
+
+    res.status(200).send(searchResultRawJSON);
+  } catch (err) {
+    res.status(403).json({ message: err.message })
   }
 });
 
